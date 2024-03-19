e8443f151ac4c8e359329c94f91f8b979e8301b0
Akshay Gopinath <akshay8055.007@gmail.com>
2024-03-19 17:56:31 +0000

    update pin map to not use unicode

--- src/pin_map.h
+++ src/pin_map.h
@@ -4,28 +4,28 @@
 //Pin definitions go here
 
 /*
-                                ┌────────────────────────────┐
-                                │[GND]                 [VOUT]│
-                                │[VIN]                   [VU]│
-                                │[VB]                   [IF-]│
-                                │[NR]                   [IF+]│
-                     LEDS_SPI - │[P5]                   [RD-]│
-                     LEDS_SPI - │[P6]                   [RD+]│
-                     LEDS_SPI - │[P7]                   [TD-]│
-                   LEDS_LATCH - │[P8]                   [TD+]│
-                                │[P9]                    [D-]│
-                                │[P10]                   [D+]│
-SWITCH_READING/KEYPAD_ROWS_IN - │[P11]                  [P30]│
-SWITCH_READING/KEYPAD_ROWS_IN - │[P12]                  [P29]│
-SWITCH_READING/KEYPAD_ROWS_IN - │[P13]                  [P28]│
-SWITCH_READING/KEYPAD_ROWS_IN - │[P14]                  [P27]│
-                       LCD_RS - │[P15]                  [P26]│ - KEYPAD_COLS_OUT/SWITCH_CS
-                        LCD_E - │[P16]                  [P25]│ - KEYPAD_COLS_OUT/SWITCH_CS
-                      LCD_DB4 - │[P17]                  [P24]│ - KEYPAD_COLS_OUT/SWITCH_CS
-                      LCD_DB5 - │[P18]                  [P23]│
-                      LCD_DB6 - │[P19]                  [P22]│
-                      LCD_DB7 - │[P20] [L1][L2][L3][L4] [P21]│
-                                └────────────────────────────┘
+                                +----------------------------+
+                                |[GND]                 [VOUT]|
+                                |[VIN]                   [VU]|
+                                |[VB]                   [IF-]|
+                                |[NR]                   [IF+]|
+                     LEDS_SPI - |[P5]                   [RD-]|
+                     LEDS_SPI - |[P6]                   [RD+]|
+                     LEDS_SPI - |[P7]                   [TD-]|
+                   LEDS_LATCH - |[P8]                   [TD+]|
+                                |[P9]                    [D-]|
+                                |[P10]                   [D+]|
+SWITCH_READING/KEYPAD_ROWS_IN - |[P11]                  [P30]|
+SWITCH_READING/KEYPAD_ROWS_IN - |[P12]                  [P29]|
+SWITCH_READING/KEYPAD_ROWS_IN - |[P13]                  [P28]|
+SWITCH_READING/KEYPAD_ROWS_IN - |[P14]                  [P27]|
+                       LCD_RS - |[P15]                  [P26]| - KEYPAD_COLS_OUT/SWITCH_CS
+                        LCD_E - |[P16]                  [P25]| - KEYPAD_COLS_OUT/SWITCH_CS
+                      LCD_DB4 - |[P17]                  [P24]| - KEYPAD_COLS_OUT/SWITCH_CS
+                      LCD_DB5 - |[P18]                  [P23]|
+                      LCD_DB6 - |[P19]                  [P22]|
+                      LCD_DB7 - |[P20] [L1][L2][L3][L4] [P21]|
+                                +----------------------------+
                                      ALARM_LED      
 */
 
